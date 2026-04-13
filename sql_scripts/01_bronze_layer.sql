0a97167b1e7e7630c11726bfa439615a2c2d2572
chaitu-pixel <chaitubourothu@gmail.com>
2026-04-13 13:55:16 +0530

    Update file paths for data loading in SQL script

--- sql_scripts/01_bronze_layer.sql
+++ sql_scripts/01_bronze_layer.sql
@@ -140,7 +140,7 @@ SHOW VARIABLES LIKE 'local_infile';
 SET @Start_Time = now();
 SELECT @Start_Time AS Start_time;
 TRUNCATE TABLE bronze.crm_cust_info;
-LOAD DATA LOCAL INFILE '/Users/uday/Downloads/sql-data-warehouse-project/datasets/source_crm/cust_info.csv'
+LOAD DATA LOCAL INFILE "C:\Users\chait\Downloads\sql_data_warehouse_project-main\datasets\source_crm\cust_info.csv"
 INTO TABLE bronze.crm_cust_info
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ','
@@ -162,7 +162,7 @@ cst_create_date = CASE
   
   
   TRUNCATE TABLE bronze.crm_prd_info;
-LOAD DATA LOCAL INFILE '/Users/uday/Downloads/sql-data-warehouse-project/datasets/source_crm/prd_info.csv'
+LOAD DATA LOCAL INFILE "C:\Users\chait\Downloads\sql_data_warehouse_project-main\datasets\source_crm\prd_info.csv"
 INTO TABLE bronze.crm_prd_info
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ','
@@ -185,7 +185,7 @@ SET
                    END;
                    
     TRUNCATE TABLE bronze.crm_sales_details;
-LOAD DATA LOCAL INFILE '/Users/uday/Downloads/sql-data-warehouse-project/datasets/source_crm/sales_details.csv'
+LOAD DATA LOCAL INFILE "C:\Users\chait\Downloads\sql_data_warehouse_project-main\datasets\source_crm\sales_details.csv"
 INTO TABLE bronze.crm_sales_details
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ','
@@ -206,7 +206,7 @@ SET
   
 
 TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-LOAD DATA LOCAL INFILE '/Users/uday/Downloads/sql-data-warehouse-project/datasets/source_erp/PX_CAT_G1V2.csv'
+LOAD DATA LOCAL INFILE "C:\Users\chait\Downloads\sql_data_warehouse_project-main\datasets\source_erp\PX_CAT_G1V2.csv"
 INTO TABLE bronze.erp_px_cat_g1v2
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ','
@@ -222,7 +222,7 @@ SET
 
 
 TRUNCATE TABLE bronze.erp_loc_a101;
-LOAD DATA LOCAL INFILE '/Users/uday/Downloads/sql-data-warehouse-project/datasets/source_erp/LOC_A101.csv'
+LOAD DATA LOCAL INFILE "C:\Users\chait\Downloads\sql_data_warehouse_project-main\datasets\source_erp\LOC_A101.csv"
 INTO TABLE bronze.erp_loc_a101
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ','
@@ -236,7 +236,7 @@ SET
 
 
 TRUNCATE TABLE bronze.erp_cust_az12;
-LOAD DATA LOCAL INFILE '/Users/uday/Downloads/sql-data-warehouse-project/datasets/source_erp/CUST_AZ12.csv'
+LOAD DATA LOCAL INFILE "C:\Users\chait\Downloads\sql_data_warehouse_project-main\datasets\source_erp\CUST_AZ12.csv"
 INTO TABLE bronze.erp_cust_az12
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ','
@@ -264,4 +264,3 @@ SELECT * FROM bronze.erp_cust_az12;
 -- *************************************************************************************************************************************
 -- ======================================================*** BRONZE LAYER (END) ***=====================================================
 -- *************************************************************************************************************************************
-
